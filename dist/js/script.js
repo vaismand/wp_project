1454345a496da1c22dbb44532c263971c0f83396
David <omgemjey@gmail.com>
2019-12-15 11:53:11 +0100

    section

--- dist/js/script.js
+++ dist/js/script.js
@@ -48,6 +48,44 @@ $(document).ready(function(){
             {
             breakpoint: 1024,
             settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+                }
+            },
+            {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+                }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
+$(document).ready(function(){
+    $('.clients_slide').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 700,
+        slidesToShow: 6,
+        slidesToScroll: 2,
+        prevArrow: '<div class="slick-prev"><img src="../icons/left2.png"></div>',
+        nextArrow: '<div class="slick-next"><img src="../icons/right2.png"></div>',
+        responsive: [
+            {
+            breakpoint: 1024,
+            settings: {
                 slidesToShow: 3,
                 slidesToScroll: 3,
                 infinite: true,
